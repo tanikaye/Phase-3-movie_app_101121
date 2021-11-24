48c4ab9fb2e77e05aafd9520fa7042a092f49089
Tani Kaye <1tanikaye@gmail.com>
2021-11-23 23:57:56 -0500

    commit tree 48dd553991ff9a592505f21327a6271cc7e5a392

--- movies_app/lib/cli.rb
+++ movies_app/lib/cli.rb
@@ -1,41 +1,115 @@
+require "pry"
 
-
-def initialize_app 
+def initialize_app
     movies = ['2001: A Space Odyssey', 'The Godfather', 'Citizen Kane', 'Raiders of the Lost Ark', 'In the mood for love', 'Seven Samurai']
 
-    tickets = [{name:'rose', movie:'2001: A Space Odyssey', price:5.00},{name:'ix', movie:'The Godfather', price:5.00}]
-    
+def menu
     puts "Welcome to Flatiron Movies"
     puts "Please choose an option:"
     puts "1. List all Movies"
     puts "2. List Tickte holder names"
     puts "3. Add ticket"
     puts "0. to exit"
-    
+
     user_input = gets.strip
 
     case user_input
     when "1"
-        movies.each {|movie| puts movie }
+        movies.each { |movie| puts movie }
     when "2"
-       puts tickets.map {|ticket| ticket[:name] }
+        puts tickets.map { |ticket| ticket[:name]}
     when "3"
         puts create_ticket
-    else 
+    else
         puts "goodbye"
-    end 
-
-end 
+    end
+end
+end
 
 def create_ticket
-     puts 'name'
-     name = gets.strip
-     puts "Title"
-     title = gets.strip
+    puts "name"
+    name = gets.strip
+    puts "title"
+    title = gets.strip
 
-     {
-         name:name,
-         title:title,
-         price: 5.00
-     }
-end 
+    {
+        name:name,
+        title:title,
+        price:5.00
+
+    }
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def initialize_app
+#     movies = ['2001: A Space Odyssey', 'The Godfather', 'Citizen Kane', 'Raiders of the Lost Ark', 'In the mood for love', 'Seven Samurai']
+
+    # tickets = [{name:'rose', movie:'2001: A Space Odyssey', price:5.00},{name:'ix', movie:'The Godfather', price:5.00}]
+
+    # puts "Welcome to Flatiron Movies"
+    # puts "Please choose an option:"
+    # puts "1. List all Movies"
+    # puts "2. List Tickte holder names"
+    # puts "3. Add ticket"
+    # puts "0. to exit"
+
+#     user_input = gets.strip
+
+#     case user_input
+#     when "1"
+#         movies.each {|movie| puts movie }
+#     when "2"
+#        puts tickets.map {|ticket| ticket[:name] }
+#     when "3"
+#         puts create_ticket
+#     else
+#         puts "goodbye"
+#     end
+
+# end
+
+# def create_ticket
+#      puts 'name'
+#      name = gets.strip
+#      puts "Title"
+#      title = gets.strip
+
+#      {
+#          name:name,
+#          title:title,
+#          price: 5.00
+#      }
+# end
